858b1f7452e26eb092098eb69e9c5a7a2881c4b5
Arne van Schalkwijk <arnescha2003@gmail.com>
2023-03-13 11:53:48 +0100

    Ik heb prettier toegevoegt zodat de code volledig juist mooi is, ik heb meer content toegevoegt, alles responsie gemaakt, postcss toegevoegt, eslint toegevoegd, lint staged gedaan

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 // init Swiper:
-const swiper = new Swiper('.swiper', {
+new Swiper('.swiper', {
   // configure Swiper to use modules
   modules: [Navigation, Pagination],
 
@@ -27,15 +27,17 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
 });
 
-const accessToken = 'pk.eyJ1IjoiYXJuZXZzIiwiYSI6ImNsYWk3YTB1ajA3YjEzcXA2MHM2em44ZWEifQ.DcHCYjw-L3ZfJFPOilUgsg';
+const accessToken =
+  'pk.eyJ1IjoiYXJuZXZzIiwiYSI6ImNsYWk3YTB1ajA3YjEzcXA2MHM2em44ZWEifQ.DcHCYjw-L3ZfJFPOilUgsg';
 
 // Use an asynchronous function to get the address from the coordinates
 // De lattitude en longitude heb ik omgedraaid
 async function getAddressFromCoordinates() {
-  const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/4.410443,51.202335.json?access_token=${accessToken}`);
+  const response = await fetch(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/4.410443,51.202335.json?access_token=${accessToken}`,
+  );
   const data = await response.json();
   const address = data.features[0].place_name;
   document.getElementById('address').textContent = address;
